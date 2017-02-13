462b78b44622e264228762d4089a7a51abf189a7
unknown <emeliza.pesquera@CRI-B2L5.cri-dc.litfirepublishing.com>
2017-02-13 13:22:21 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+echo 'this is email validator' . '<br>';
+
+$username="root";
+$password="";
+$database="test";
+
+/* if(mysql_connect('localhost',$username,$password)) {
+	echo "connected to mysql";
+} else {
+	echo "cannot connect to mysql";
+} */
+
+@mysql_connect('localhost',$username,$password) or die("cannot connect to mysql");
+
+@mysql_select_db($database) or die( "Unable to select database");
+
+$query = "Select * from emails";
+$result = mysql_query($query);
+
+$emails = array();
+while ($email =  mysql_fetch_assoc($result))
+{
+	$emails[] = $email['email'];
+}
+foreach ($emails as $email)
+{
+	if (filter_var($email, FILTER_VALIDATE_EMAIL) == FALSE) {
+		echo $email . '<br>';
+	}
+}
+		
+mysql_close();
+?>
